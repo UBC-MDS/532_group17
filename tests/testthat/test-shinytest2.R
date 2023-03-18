9a9601c339de9f2adcd9076ac8915625c9c9c3f0
LisaSeq <112457048+LisaSeq@users.noreply.github.com>
2023-03-18 10:40:29 -0700

    Deleted all tests and added 1 new one.

--- tests/testthat/test-shinytest2.R
+++ tests/testthat/test-shinytest2.R
@@ -1,31 +1,8 @@
 library(shinytest2)
 
-test_that("{shinytest2} recording: Art_type_filter", {
-  app <- AppDriver$new(name = "Art_type_filter", height = 714, width = 1235)
-  app$set_inputs(type = "Totem pole")
-  app$set_inputs(type = c("Totem pole", "Other"))
-  app$set_inputs(tabset = "Art Type")
+test_that("{shinytest2} recording: VanArt_defaultscreen", {
+  app <- AppDriver$new(variant = platform_variant(), name = "VanArt_defaultscreen", 
+      height = 714, width = 1235)
   app$expect_values()
-})
-
-
-test_that("{shinytest2} recording: Test_Neighbourhood_filter", {
-  app <- AppDriver$new(name = "Test_Neighbourhood_filter", height = 714, width = 1235)
-  app$set_inputs(neighbourhood = "Fairview")
-  app$set_inputs(tabset = "Neighbourhood")
-  app$expect_values()
-  app$set_inputs(tabset = "Art Type")
-  app$expect_values()
-  app$set_inputs(tabset = "Year Installed")
-  app$expect_values()
-})
-
-
-
-test_that("{shinytest2} recording: Test_default_view", {
-  app <- AppDriver$new(name = "Test_default_view", height = 714, width = 1235)
-  app$expect_values()
-  app$set_inputs(tabset = "Art Type")
-  app$set_inputs(tabset = "Neighbourhood")
-  app$set_inputs(tabset = "Year Installed")
+  app$expect_screenshot()
 })
