3571d2f74832f60b350747f97f694002057bccd5
LisaSeq <112457048+LisaSeq@users.noreply.github.com>
2023-03-18 12:10:17 -0700

    Updated Test scripts with variant argument

--- tests/testthat/test-shinytest2.R
+++ tests/testthat/test-shinytest2.R
@@ -1,14 +1,14 @@
 library(shinytest2)
 
 test_that("{shinytest2} recording: Test_Art_Type_Filter", {
-  app <- AppDriver$new(name = "Test_Art_Type_Filter", seed = 532, height = 714, width = 1235)
+  app <- AppDriver$new(variant = platform_variant(), name = "Test_Art_Type_Filter", seed = 532, height = 714, width = 1235)
   app$set_inputs(type = "Figurative")
   app$expect_values()
 })
 
 
 test_that("{shinytest2} recording: Test_Neighbourhood", {
-  app <- AppDriver$new(name = "Test_Neighbourhood", seed = 532, height = 714, width = 1235)
+  app <- AppDriver$new(variant = platform_variant(), name = "Test_Neighbourhood", seed = 532, height = 714, width = 1235)
   app$set_inputs(neighbourhood = "Fairview")
   app$expect_values()
 })
@@ -16,6 +16,6 @@ test_that("{shinytest2} recording: Test_Neighbourhood", {
 
 
 test_that("{shinytest2} recording: Test_Default_view", {
-  app <- AppDriver$new(name = "Test_Default_view", seed = 532, height = 714, width = 1235)
+  app <- AppDriver$new(variant = platform_variant(), name = "Test_Default_view", seed = 532, height = 714, width = 1235)
   app$expect_values()
 })
